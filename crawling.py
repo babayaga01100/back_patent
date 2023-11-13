dff41203bef02fcaf5af5b1b6b0083ed131ebb6d
babayaga01100 <babayaga01100@gmail.com>
2023-11-13 17:17:24 +0900

    final commit

--- crawling.py
+++ crawling.py
@@ -14,13 +14,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.relative_locator import locate_with
+
 from patentNotice.models import PatentNotice, PatentNoticeDate
 import logging
 from datetime import datetime
 
 # from patentAttorneys.models import TestNotice  # Django 모델 임포트
 
-logger = logging.getLogger("crawler")
+# logger = logging.getLogger("crawler")
 
 
 def crawlling():
@@ -44,40 +46,97 @@ def crawlling():
     # date
     # date_element = driver.find_element(By.XPATH, '//*[@class="bbs_tit"]/a')
     # date_text = date_element.get_attribute("title")
-    # date_element = driver.find_elements(By.XPATH, '//h3[@id="popTitle"]')
+    # date_element = driver.find_element(By.XPATH, '//h3[@id="popTitle"]')
     # date_text = date_element.text
 
 
     # title 링크 클릭
-    title_elements = driver.find_elements(By.XPATH, '//*[@class="bbs_tit"]/a')
-
-    for title_element in title_elements:
-        title_text = title_element.text  # title 텍스트 가져오기
-        # date_text = title_text.split('(')[-1].strip(')')
-        title_element.click()
-
-        # 새 탭으로 전환
-        driver.switch_to.window(driver.window_handles[-1])
-
-        image = driver.find_elements(By.TAG_NAME, '//*[@id="htmlCont"]/table/tbody/tr[2]/td/a/img')
-        image_jpg = image.get_attribute("src")
+    # title_elements = driver.find_element(By.XPATH, '//*[@class="bbs_tit"]/a')
+    
+    while True:
+        for i in range(1, 11):
+            # 공지 제목 가져오기
+            try:
+                driver.find_element(By.XPATH, '//*[@id="content"]/article/div[3]/table/tbody/tr[' + str(i) + ']/td[2]/a').click()
+            except:
+                return
+            
+            # 공지 내용 가져오기
+            title = driver.find_element(By.XPATH, '//*[@id="popTitle"]').text
         
-        # 이미지 url 크롤링
-        image_element = driver.find_element(By.TAG_NAME, "img")
-        image_url = image_element.get_attribute("src")
+            # 데이터베이스에 저장
+            PatentNoticeDate(title=title).save()
+            
+            title_date = PatentNoticeDate.objects.latest('id') # 가장 나중에 저장된 인덱스 가져오기
 
-        # 데이터베이스에 저장
-        date = PatentNoticeDate(title=title_text)
-        date.save()
+            # 이미지 jpg 크롤링
+            # image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[2]/td/a/img').get_attribute('src')
+            
+            # 이미지 url 크롤링
+            # image_url = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[2]/td/a').get_attribute('href')
         
-        # date = PatentNoticeDate(title=title_text, date=date_text)
-        print(title_text)
-        print(image_jpg)
-        news = PatentNotice(title=date, image=image_jpg, image_url=image_url)
-        news.save()
+            # image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[3]/td[1]/a/img').get_attribute('src')
 
-        # 이전 탭으로 전환
-        driver.switch_to.window(driver.window_handles[0])
+            try:
+                for i in (2, 7):
+                    # 이미지 jpg 크롤링
+                    image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[' + str(i) + ']/td/a/img').get_attribute('src')
+                    
+                    # 이미지 url 크롤링
+                    image_url = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[' + str(i) + ']/td/a').get_attribute('href')
+                
+                    # 데이터베이스에 저장
+                    # date = PatentNoticeDate(title=title).save()
+                    PatentNotice(title=title_date, image=image, image_url=image_url).save()
+            
+                for i in range(1, 3):
+                    image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[3]/td[' + str(i) + ']/a/img').get_attribute('src')
+                    image_url = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[3]/td[' + str(i) + ']/a').get_attribute('href')
+            
+                    # 데이터베이스에 저장
+                    # date = PatentNoticeDate(title=title).save()
+                    PatentNotice(title=title_date, image=image, image_url=image_url).save()
+            
+                # image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[7]/td/a/img').get_attribute('src')
+                # image_url = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[7]/td/a').get_attribute('href')
+
+                image = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[8]/td/table/tbody/tr/td/a/img').get_attribute('src')
+                image_url = driver.find_element(By.XPATH, '//*[@id="htmlCont"]/table/tbody/tr[8]/td/table/tbody/tr/td/a').get_attribute('href')
+                                                                                                                                        
+                PatentNotice(title=title_date, image=image, image_url=image_url).save()
+            except:
+                pass
+            
+            # x 눌러서 공지 화면 돌아가기
+            driver.find_element(By.XPATH, '//*[@id="content"]/article/div[4]/div[2]/div[1]/a').click()
+
+        try:
+            driver.find_element(locate_with(By.TAG_NAME, 'a').to_right_of({By.XPATH: '//*[@id="content"]/article/div[5]/strong'})).click()
+
+        except:
+            break
+    return
+
+    # print(title_date)
+    # print(news)
+    # news = PatentNotice(title=title_date, image=image, image_url=image_url)
+    # news.save()
+    # 새 탭으로 전환
+    # driver.switch_to.window(driver.window_handles[-1])
+    
+    
+    # 이전 탭으로 전환
+    # driver.switch_to.window(driver.window_handles[0])
+    
+    # 다음 목록으로 이동하기
+    # try:
+    #     driver.find_element(
+    #         locate_with(By.TAG_NAME, "a").to_right_of(
+    #             {By.XPATH: '//*[@id="content"]/article/div[4]/div[2]/div[1]/a'}
+    #         )
+    #     ).click()
+    # except:
+    #     break
 
     # response = requests.get(url)
     # soup = BeautifulSoup(response.text, 'html.parser')
--- patentNotice/views.py
+++ patentNotice/views.py
@@ -1,15 +1,28 @@
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import PatentNotice, PatentNoticeDate
 from .serializers import PatenetNoticeBaseModelSerializer,PatenetNoticeDateBaseModelSerializer
 
+# Create your views here.
 # 공지사항 리스트 보기
 class PatentNoticeListView(generics.ListAPIView):
     queryset = PatentNotice.objects.all()
     serializer_class = PatenetNoticeBaseModelSerializer
 
-class PatentNoticeDateListView(generics.ListAPIView):
-    queryset = PatentNoticeDate.objects.all()
-    serializer_class = PatenetNoticeDateBaseModelSerializer
-
-# Create your views here.
+# class PatentNoticeDateListView(generics.ListAPIView):
+    # queryset = PatentNoticeDate.objects.all()
+    # serializer_class = PatenetNoticeDateBaseModelSerializer
+class PatentNoticeDateListView(APIView):
+    def get(self, request, pk):
+        try:
+            patentNotice = PatentNotice.objects.get(id=pk)
+        except PatentNotice.DoesNotExist:
+            return Response({'detail': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        queryset = PatentNoticeDate.objects.filter(title=PatentNoticeDate)
+        queryset_serializer = PatenetNoticeBaseModelSerializer(queryset, many=True) # many : 중복 표현 값에 대한 list를 받고자 할 때 사용
+        
+        return Response(queryset_serializer.data, status=status.HTTP_200_OK)
